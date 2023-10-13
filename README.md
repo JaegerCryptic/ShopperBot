e4b7d80ec18e36c6778735ed9c3b74f510294faf
Kyle Kent <kkent10@yahoo.com>
2023-10-13 22:44:02 +1000

    Added to initial state

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ShopperBot
 Basic Python Bot for Securing pre-order items before scalpers.
+
+This bot, while in breach of most websites terms of service, will not be used to facilitate scalping. Restrictions are in place to ensure such a thing is impossible. 
+
+This program is intended as a way to circumvent scalping, not a tool to encourage it.
--- setup.py
+++ setup.py
@@ -11,11 +11,23 @@ setup(
     packages=find_packages(),
     install_requires=[
         "selenium>=3.141.0",
+        "requests",
+        "beautifulsoup4",
+        "webdriver-manager",
+        "retry",
     ],
     extras_require={
         "dev": [
             "pylint",
             "black",
+            "bandit",
+            "coverage",
         ],
     },
+    entry_points={"console_scripts": ["shoppingbot=shoppingbot.main:run"]},
+    author="Kyle",
+    author_email="kkent10@yahoo.com",
+    description="A bot designed to buy pre-order items on websites.",
+    license="MIT",
+    url="https://github.com/JaegerCryptic/ShoppingBot",
 )
